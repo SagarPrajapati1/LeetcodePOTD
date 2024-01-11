5705027341b58b737da81d3c4e67db1b1aa7c829
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-01-11 17:10:17 +0530

    Maximum_Difference_Between_Node_and_Ancestor

---
+++ Jan24/Maximum_Difference_Between_Node_and_Ancestor.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int maxAncestorDiff(TreeNode* root, int mini = 100000, int maxi = 0) {
+ 
+        if(root == nullptr) return maxi - mini;
+
+        mini = min(mini, root -> val);
+        maxi = max(maxi, root -> val);
+        return max(maxAncestorDiff(root->left, mini, maxi), maxAncestorDiff(root -> right, mini, maxi));
+    }
+};
