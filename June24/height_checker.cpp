2ed0c1a24956417c9ab106f4183e9150b4f6e098
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-06-10 19:37:20 +0530

    height_checker

---
+++ June24/height_checker.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int map[101]; // heights go from 1 to 100
+        for(int h: heights) ++map[h];
+        
+        int res = 0;
+        int h_ptr = 1;
+        
+        for(int h: heights) {
+            while(map[h_ptr] == 0) ++h_ptr;
+            
+            if(h_ptr != h) {
+                ++res;
+            }
+            
+            --map[h_ptr];
+        }
+        
+        return res;
+    }
+};
