a7d63a736ce07dcd973c1e206260d50bd8c236e4
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-09-19 22:10:17 +0530

    different_ways_to_add_parantheses

---
+++ Sep24/different_ways_to_add_parantheses.cpp
@@ -0,0 +1,95 @@
+class Solution {
+public:
+    // function to get the result of the operation
+    int perform(int x, int y, char op) {
+        if(op == '+') return x + y;
+        if(op == '-') return x - y;
+        if(op == '*') return x * y;
+        return 0;
+    }
+    
+    vector<int> diffWaysToCompute(string exp) {
+        
+        vector<int> results;
+        bool isNumber = 1;
+    
+        for(int i = 0; i < exp.length(); i++) {
+            // check if current character is an operator
+            if(!isdigit(exp[i])) {
+                
+                // if current character is not a digit then
+                // exp is not purely a number
+                isNumber = 0;
+                
+                // list of first operands
+                vector<int> left = diffWaysToCompute(exp.substr(0, i));
+                
+                // list of second operands
+                vector<int> right = diffWaysToCompute(exp.substr(i + 1));
+                
+                // performing operations
+                for(int x : left) {
+                    for(int y : right) {
+                        int val = perform(x, y, exp[i]);
+                        results.push_back(val);
+                    }
+                }
+                
+            }
+        }
+        
+        if(isNumber == 1) results.push_back(stoi(exp));
+        return results;
+    }
+};
+
+
+
+class Solution {
+public:
+	vector<int> diffWaysToCompute(string input) {
+		unordered_map<string, vector<int>> dpMap;
+		return computeWithDP(input, dpMap);
+	}
+
+	vector<int> computeWithDP(string input, unordered_map<string, vector<int>> &dpMap) {
+		vector<int> result;
+		int size = input.size();
+		for (int i = 0; i < size; i++) {
+			char cur = input[i];
+			if (cur == '+' || cur == '-' || cur == '*') {
+				// Split input string into two parts and solve them recursively
+				vector<int> result1, result2;
+				string substr = input.substr(0, i);
+				// check if dpMap has the result for substr
+				if (dpMap.find(substr) != dpMap.end())
+					result1 = dpMap[substr];
+				else
+					result1 = computeWithDP(substr, dpMap);
+
+				substr = input.substr(i + 1);
+				if (dpMap.find(substr) != dpMap.end())
+					result2 = dpMap[substr];
+				else
+					result2 = computeWithDP(substr, dpMap);
+				
+				for (auto n1 : result1) {
+					for (auto n2 : result2) {
+						if (cur == '+')
+							result.push_back(n1 + n2);
+						else if (cur == '-')
+							result.push_back(n1 - n2);
+						else
+							result.push_back(n1 * n2);
+					}
+				}
+			}
+		}
+		// if the input string contains only number
+		if (result.empty())
+			result.push_back(atoi(input.c_str()));
+		// save to dpMap
+		dpMap[input] = result;
+		return result;
+	}
+};
